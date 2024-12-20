1e6d7dd69d0630d55d4b8af638bd0a8c164f34bf
tetto <tetto@cern.ch>
2024-12-20 06:11:52 +0100

    Add initial files

--- src/classes.cc
+++ src/classes.cc
@@ -3,14 +3,3 @@
 #include "DataFormats/Common/interface/DetSetVectorNew.h"
 #include "DataFormats/SiPixelDigi/interface/SiPixelDigisSoA.h"
 
-// Explicit template instantiations
-template class edmNew::DetSetVector<SiPixelDigisSoA>;
-template class edm::Wrapper<edmNew::DetSetVector<SiPixelDigisSoA>>;  // Wrapper instantiation
-
-namespace {
-  struct dictionary {
-    SiPixelDigisSoA dummy1;  // Instantiate SiPixelDigisSoA
-    edmNew::DetSetVector<SiPixelDigisSoA> dummy2;  // Instantiate DetSetVector<SiPixelDigisSoA>
-    edm::Wrapper<edmNew::DetSetVector<SiPixelDigisSoA>> dummy3;  // Instantiate Wrapper
-  };
-}
